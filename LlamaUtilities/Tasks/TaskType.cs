b455f602b74e27ca47401b81f318444a2a2f20a7
nt153133 <52430037+nt153133@users.noreply.github.com>
2021-11-27 02:52:06 -0500

    Initial commit

---
+++ LlamaUtilities/Tasks/TaskType.cs
@@ -0,0 +1,19 @@
+﻿namespace LlamaBotBases.LlamaUtilities.Tasks
+{
+    public enum TaskType
+    {
+        None,
+        MateriaRemove,
+        AutoFollow,
+        Reduce,
+        Desynth,
+        Hunts,
+        Extract,
+        Coffers,
+        Housing,
+        CustomDeliveries,
+        GcTurnin,
+        Retainers,
+        FCWorkshop
+    }
+}
