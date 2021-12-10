e876c72bcd7e8c119a2de12c16385b0071f71679
nt153133 <52430037+nt153133@users.noreply.github.com>
2021-12-10 06:46:28 -0500

    BuyInclusionShopItem

---
+++ OrderbotTags/BuyInclusionShopItem.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel;
+using System.Threading.Tasks;
+using Clio.XmlEngine;
+using TreeSharp;
+
+namespace LlamaUtilities.OrderbotTags
+{
+    [XmlElement("BuyInclusionShopItem")]
+    public class BuyInclusionShopItem: LLProfileBehavior
+    {
+        private bool _isDone;
+
+        [XmlAttribute("ItemId")]
+        public int ItemId { get; set; }
+
+        [XmlAttribute("Count")]
+        [DefaultValue(-1)]
+        public int Count { get; set; }
+
+        public override bool HighPriority => true;
+
+        public override bool IsDone => _isDone;
+
+        public BuyInclusionShopItem() : base() { }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnDone()
+        {
+        }
+
+        protected override void OnResetCachedDone()
+        {
+            _isDone = false;
+        }
+
+        protected override Composite CreateBehavior()
+        {
+            return new ActionRunCoroutine(r => BuyKnownItem(ItemId, Count));
+        }
+
+        private async Task BuyKnownItem(int itemId, int Count)
+        {
+            await LlamaLibrary.Helpers.InclusionShopHelper.BuyItemGoToNpc((uint)itemId, Count);
+
+            _isDone = true;
+        }
+    }
+}
