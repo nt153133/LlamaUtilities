9d0ca88c361af3b64e4537ef2e7513238ddbcc85
domesticwarlord86 <57776315+domesticwarlord86@users.noreply.github.com>
2022-01-12 14:16:46 -0600

    Added GCRankUp

---
+++ OrderbotTags/GCRankUp.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using Clio.XmlEngine;
+using LlamaLibrary.Logging;
+using TreeSharp;
+
+namespace LlamaUtilities.OrderbotTags
+{
+    [XmlElement("GCRankUp")]
+    public class GCRankUp : LLProfileBehavior
+    {
+        private bool _isDone;
+
+
+
+        public override bool HighPriority => true;
+
+        public override bool IsDone => _isDone;
+
+        public GCRankUp() : base() { }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnDone()
+        {
+        }
+
+        protected override void OnResetCachedDone()
+        {
+            _isDone = false;
+        }
+
+        protected override Composite CreateBehavior()
+        {
+            return new ActionRunCoroutine(r => DoGCRankUp());
+        }
+
+        private async Task DoGCRankUp()
+        {
+            await LlamaLibrary.Helpers.GrandCompanyHelper.GoGCRankUp();
+						
+						 _isDone = true;
+
+        }
+    }
+}
