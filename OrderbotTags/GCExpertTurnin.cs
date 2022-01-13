58d56943e1e0eba829e16431c1322aaca6b7a33f
domesticwarlord86 <57776315+domesticwarlord86@users.noreply.github.com>
2022-01-13 09:29:22 -0600

    Added GCExpertTurnin.cs

---
+++ OrderbotTags/GCExpertTurnin.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using Clio.XmlEngine;
+using LlamaLibrary.Logging;
+using TreeSharp;
+
+namespace LlamaUtilities.OrderbotTags
+{
+    [XmlElement("GCExpertTurnin")]
+    public class GCExpertTurnin : LLProfileBehavior
+    {
+        private bool _isDone;
+
+
+
+        public override bool HighPriority => true;
+
+        public override bool IsDone => _isDone;
+
+        public GCExpertTurnin() : base() { }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnDone()
+        {
+        }
+
+        protected override void OnResetCachedDone()
+        {
+            _isDone = false;
+        }
+
+        protected override Composite CreateBehavior()
+        {
+            return new ActionRunCoroutine(r => DoGCExpertTurnin());
+        }
+
+        private async Task DoGCExpertTurnin()
+        {
+            await LlamaLibrary.Helpers.GrandCompanyHelper.GCHandInExpert();
+						
+						 _isDone = true;
+
+        }
+    }
+}
