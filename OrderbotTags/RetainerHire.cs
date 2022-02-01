e09fb9720507b30be9dd3c0d7140a4f49429c99d
nt153133 <52430037+nt153133@users.noreply.github.com>
2022-02-01 03:18:04 -0500

    Hire tag

---
+++ OrderbotTags/RetainerHire.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel;
+using System.Threading.Tasks;
+using Clio.XmlEngine;
+using LlamaLibrary.Utilities;
+using TreeSharp;
+
+namespace LlamaUtilities.OrderbotTags
+{
+    [XmlElement("LLReduce")]
+    public class RetainerHire : LLProfileBehavior
+    {
+        private bool _isDone;
+
+        public override bool HighPriority => true;
+
+        public override bool IsDone => _isDone;
+
+        [XmlAttribute("OnlyLimsa")]
+        [DefaultValue(false)]
+        public bool OnlyLimsa { get; set; } = false;
+
+        public RetainerHire() : base()
+        {
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnDone()
+        {
+        }
+
+        protected override void OnResetCachedDone()
+        {
+            _isDone = false;
+        }
+
+        protected override Composite CreateBehavior()
+        {
+            return new ActionRunCoroutine(r => RetainerHireTask());
+        }
+
+        private async Task RetainerHireTask()
+        {
+            await LlamaLibrary.Utilities.RetainerHire.HireAllRetainers(OnlyLimsa);
+
+            _isDone = true;
+        }
+    }
+}
