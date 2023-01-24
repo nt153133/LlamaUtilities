d11aea6ef8978572fac03d607c6ee06c3ecea3e3
domesticwarlord86 <domesticwarlord86@gmail.com>
2023-01-23 21:56:43 -0600

    Update LLToast.cs
    Added color and font options

--- OrderbotTags/LLToast.cs
+++ OrderbotTags/LLToast.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Threading.Tasks;
 using Clio.XmlEngine;
 using ff14bot;
@@ -14,6 +15,31 @@ namespace LlamaUtilities.OrderbotTags
         [XmlAttribute("Message")]
         [XmlAttribute("message")]
         public string Message { get; set; }
+
+        [XmlAttribute("Duration")]
+        [XmlAttribute("Duration")]
+        [DefaultValue(25000)]
+        public int DisplayTime { get; set; }
+
+        [XmlAttribute("Red")]
+        [XmlAttribute("red")]
+        [DefaultValue(29)]
+        public int Red { get; set; }
+
+        [XmlAttribute("Blue")]
+        [XmlAttribute("blue")]
+        [DefaultValue(213)]
+        public int Blue { get; set; }
+
+        [XmlAttribute("Green")]
+        [XmlAttribute("green")]
+        [DefaultValue(226)]
+        public int Green { get; set; }
+
+        [XmlAttribute("Font")]
+        [XmlAttribute("font")]
+        [DefaultValue("Gautami")]
+        public string Font { get; set; }
 
         public override bool HighPriority => true;
 
@@ -41,7 +67,7 @@ namespace LlamaUtilities.OrderbotTags
 
         private Task SendToast(string message)
         {
-            Core.OverlayManager.AddToast(() => "" + message, TimeSpan.FromMilliseconds(25000), System.Windows.Media.Color.FromRgb(29, 213, 226), System.Windows.Media.Color.FromRgb(13, 106, 175), new System.Windows.Media.FontFamily("Gautami"));
+            Core.OverlayManager.AddToast(() => $"" + message, TimeSpan.FromMilliseconds(DisplayTime), System.Windows.Media.Color.FromRgb((byte)Red, (byte)Green, (byte)Blue), System.Windows.Media.Color.FromRgb(13, 106, 175), new System.Windows.Media.FontFamily(Font));
 
             _isDone = true;
             return Task.CompletedTask;
