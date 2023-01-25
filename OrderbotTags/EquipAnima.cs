26e8ebffab5e633b4e2f2d716b90c6793b8a29df
domesticwarlord86 <domesticwarlord86@gmail.com>
2023-01-25 15:32:43 -0600

    Added tags

---
+++ OrderbotTags/EquipAnima.cs
@@ -0,0 +1,166 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Buddy.Coroutines;
+using Clio.XmlEngine;
+using ff14bot;
+using ff14bot.Helpers;
+using ff14bot.Enums;
+using ff14bot.Managers;
+using ff14bot.RemoteWindows;
+using LlamaLibrary.Extensions;
+using TreeSharp;
+
+namespace LlamaUtilities.OrderbotTags
+{
+    [XmlElement("EquipAnima")]
+    public class EquipAnima : LLProfileBehavior
+    {
+        private bool _isDone;
+
+        public override bool HighPriority => true;
+
+        public EquipAnima() : base()
+        {
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        public static Dictionary<ClassJobType, uint[]> AnimaRelicWeapons = new Dictionary<ClassJobType, uint[]>()
+        {
+            { ClassJobType.Astrologian, new uint[] { 13623, 13609, 13235, 14882, 15235, 15249, 15263, 16062 } },
+            { ClassJobType.Bard, new uint[] { 13615, 13601, 13227, 14874, 15227, 15241, 15255, 16054 } },
+            { ClassJobType.BlackMage, new uint[] { 13620, 13606, 13232, 14879, 15232, 15246, 15260, 16059 } },
+            { ClassJobType.DarkKnight, new uint[] { 13617, 13603, 13229, 14876, 15229, 15243, 15257, 16056 } },
+            { ClassJobType.Dragoon, new uint[] { 13614, 13600, 13226, 14873, 15226, 15240, 15254, 16053 } },
+            { ClassJobType.Machinist, new uint[] { 13618, 13604, 13230, 14877, 15230, 15244, 15258, 16057 } },
+            { ClassJobType.Monk, new uint[] { 13612, 13598, 13224, 14871, 15224, 15238, 15252, 16051 } },
+            { ClassJobType.Ninja, new uint[] { 13616, 13602, 13228, 14875, 15228, 15242, 15256, 16055 } },
+            { ClassJobType.Paladin, new uint[] { 13611, 13597, 13223, 14870, 15223, 15237, 15264, 16063 } },
+            { ClassJobType.Scholar, new uint[] { 13622, 13608, 13234, 14881, 15234, 15248, 15262, 16061 } },
+            { ClassJobType.Summoner, new uint[] { 13621, 13607, 13233, 14880, 15233, 15247, 15261, 16060 } },
+            { ClassJobType.Warrior, new uint[] { 13613, 13599, 13225, 14872, 15225, 15239, 15253, 16052 } },
+            { ClassJobType.WhiteMage, new uint[] { 13619, 13605, 13231, 14878, 15231, 15245, 15259, 16058 } },
+        };
+
+        public static uint[] AnimaRelicOffhands = new uint[] { 13624, 13610, 13236, 14870, 15236, 15250, 15251, 16050 };
+
+        protected override void OnDone()
+        {
+        }
+
+        protected override void OnResetCachedDone()
+        {
+            _isDone = false;
+        }
+
+        protected override Composite CreateBehavior()
+        {
+            return new ActionRunCoroutine(r => EquipAnimaTask());
+        }
+
+        private async Task EquipAnimaTask()
+        {
+            var mainhand = InventoryManager.GetBagByInventoryBagId(InventoryBagId.EquippedItems)[EquipmentSlot.MainHand];
+            if (AnimaRelicWeapons[Core.Me.CurrentJob].Contains(mainhand.RawItemId))
+            {
+                Logging.WriteDiagnostic("Anima Relic Weapon Already Equipped");
+            }
+            else
+            {
+                while (!AnimaRelicWeapons[Core.Me.CurrentJob].Contains(mainhand.RawItemId))
+                {
+                    if (Core.Me.InCombat)
+                    {
+                        Logging.WriteDiagnostic("We are in combat and can't switch weapons.");
+                        Logging.WriteDiagnostic("Waiting 10 seconds or until combat drops");
+                        await Coroutine.Wait(10000, () => Core.Me.InCombat);
+                        while (Core.Me.InCombat)
+                        {
+                            Logging.WriteDiagnostic("Combat didn't end after 10 seconds. Trying again.");
+                            await Coroutine.Wait(10000, () => Core.Me.InCombat);
+                        }
+                    }
+
+                    Logging.WriteDiagnostic("Anima Relic Weapon Not already Equipped");
+                    var item1 = InventoryManager.FilledInventoryAndArmory.FirstOrDefault(i => AnimaRelicWeapons[Core.Me.CurrentJob].Contains(i.RawItemId));
+                    if (item1 != default(BagSlot))
+                    {
+                        item1.Move(mainhand);
+                        await BagSlotExtensions.BagSlotNotFilledWait(item1);
+                        await Coroutine.Wait(10000, () => AnimaRelicWeapons[Core.Me.CurrentJob].Contains(mainhand.RawItemId));
+                        if (!AnimaRelicWeapons[Core.Me.CurrentJob].Contains(mainhand.RawItemId))
+                        {
+                            Logging.WriteDiagnostic("Anima Relic equipping failed");
+                        }
+                        else
+                        {
+                            Logging.WriteDiagnostic($"Now wearing {mainhand.Name}");
+                        }
+                    }
+                    else
+                    {
+                        Logging.WriteDiagnostic("No Anima Relic Weapon Found");
+                        _isDone = true;
+                        return;
+                    }
+                }
+            }
+
+            if (Core.Me.CurrentJob == ClassJobType.Paladin)
+            {
+                var offhand = InventoryManager.GetBagByInventoryBagId(InventoryBagId.EquippedItems)[EquipmentSlot.OffHand];
+                if (AnimaRelicOffhands.Contains(offhand.RawItemId))
+                {
+                    Logging.WriteDiagnostic("Anima Relic Offhand Equipped");
+                }
+                else
+                {
+                    while (!AnimaRelicOffhands.Contains(offhand.RawItemId))
+                    {
+                        if (Core.Me.InCombat)
+                        {
+                            Logging.WriteDiagnostic("We are in combat and can't switch weapons.");
+                            Logging.WriteDiagnostic("Waiting 10 seconds or until combat drops");
+                            await Coroutine.Wait(10000, () => Core.Me.InCombat);
+                            while (Core.Me.InCombat)
+                            {
+                                Logging.WriteDiagnostic("Combat didn't end after 10 seconds. Trying again.");
+                                await Coroutine.Wait(10000, () => Core.Me.InCombat);
+                            }
+                        }
+
+                        Logging.WriteDiagnostic("Paladin Anima Relic Offhand Not Equipped");
+                        var item2 = InventoryManager.FilledInventoryAndArmory.FirstOrDefault(i => AnimaRelicOffhands.Contains(i.RawItemId));
+                        if (item2 != default(BagSlot))
+                        {
+                            item2.Move(offhand);
+                            await BagSlotExtensions.BagSlotNotFilledWait(item2);
+                            await Coroutine.Wait(10000, () => AnimaRelicOffhands.Contains(offhand.RawItemId));
+                            if (!AnimaRelicOffhands.Contains(offhand.RawItemId))
+                            {
+                                Logging.WriteDiagnostic("Paladin Anima Relic Offhand equipping failed");
+                            }
+                            else
+                            {
+                                Logging.WriteDiagnostic($"Now wearing {offhand.Name}");
+                            }
+                        }
+                        else
+                        {
+                            Logging.WriteDiagnostic("No Anima Relic Offhand Found");
+                            _isDone = true;
+                            return;
+                        }
+                    }
+                }
+            }
+
+            _isDone = true;
+        }
+
+        public override bool IsDone => _isDone;
+    }
+}
---
+++ OrderbotTags/PickUpAnimaQuest.cs
@@ -0,0 +1,195 @@
+﻿using System.Threading.Tasks;
+using Buddy.Coroutines;
+using Clio.XmlEngine;
+using ff14bot;
+using ff14bot.Helpers;
+using ff14bot.Managers;
+using ff14bot.Navigation;
+using ff14bot.RemoteWindows;
+using TreeSharp;
+
+namespace LlamaUtilities.OrderbotTags
+{
+    [XmlElement("PickupAnimaQuest")]
+    public class PickupAnimaQuest : LLProfileBehavior
+    {
+        private bool _isDone;
+
+        [XmlAttribute("QuestId")]
+        [XmlAttribute("QuestID")]
+        public int QuestId { get; set; }
+
+        [XmlAttribute("NpcId")]
+        [XmlAttribute("NpcID")]
+        [XmlAttribute("NPCID")]
+        public int NpcId { get; set; }
+
+        public override bool HighPriority => true;
+
+        public override bool IsDone => _isDone;
+
+        public PickupAnimaQuest() : base()
+        {
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnDone()
+        {
+        }
+
+        protected override void OnResetCachedDone()
+        {
+            _isDone = false;
+        }
+
+        protected override Composite CreateBehavior()
+        {
+            return new ActionRunCoroutine(r => PickUpAnimeWeaponQuestTask());
+        }
+
+        private async Task PickUpAnimeWeaponQuestTask()
+        {
+            var npcId = GameObjectManager.GetObjectByNPCId((uint) NpcId);
+            var QuestName = DataManager.GetLocalizedQuestName(QuestId);
+
+            if (npcId == null)
+            {
+                Logging.WriteDiagnostic($"Couldn't find {npcId.Name}, exiting'.");
+                _isDone = true;
+                return;
+            }
+
+            if (QuestLogManager.HasQuest(QuestId))
+            {
+                Logging.WriteDiagnostic($"We already have quest {QuestName}, exiting'.");
+                _isDone = true;
+                return;
+            }
+
+            if (!npcId.IsWithinInteractRange)
+
+            {
+                var _target = npcId.Location;
+                Navigator.PlayerMover.MoveTowards(_target);
+                while (_target.Distance2D(Core.Me.Location) >= 4)
+                {
+                    Navigator.PlayerMover.MoveTowards(_target);
+                    await Coroutine.Sleep(100);
+                }
+
+                Navigator.PlayerMover.MoveStop();
+            }
+
+            npcId.Interact();
+
+            await Coroutine.Wait(10000, () => SelectIconString.IsOpen);
+
+            if (!SelectIconString.IsOpen)
+            {
+                npcId.Interact();
+                await Coroutine.Wait(10000, () => SelectIconString.IsOpen);
+                if (!SelectIconString.IsOpen)
+                {
+                    Logging.WriteDiagnostic($"Interacting with {npcId.Name} didn't happen, exiting'.");
+                    _isDone = true;
+                    return;
+                }
+            }
+
+            if (QuestLogManager.IsQuestCompleted((uint) QuestId))
+            {
+                if (SelectIconString.IsOpen)
+                {
+                    if (QuestLogManager.IsQuestCompleted(67940))
+                    {
+                        Logging.WriteDiagnostic("Relic weapon completed before.");
+                        Logging.WriteDiagnostic("Choosing 'Anima Weapon Recreation'.");
+                        SelectIconString.ClickSlot(0);
+                        await Coroutine.Wait(10000, () => !SelectIconString.IsOpen);
+                    }
+                    else
+                    {
+                        Logging.WriteDiagnostic("Never completed Anima before.");
+                        Logging.WriteDiagnostic("Choosing 'Anima Weapon Recreation'.");
+                        SelectIconString.ClickSlot(1);
+                        await Coroutine.Wait(10000, () => !SelectIconString.IsOpen);
+                    }
+
+                    await Coroutine.Wait(10000, () => SelectString.IsOpen);
+                    {
+                        Logging.WriteDiagnostic($"Choosing '{QuestName}'.");
+                        if (!SelectString.ClickLineContains($"{QuestName}"))
+                        {
+                            Logging.WriteDiagnostic($"We can't find quest {QuestName}, exiting'.");
+                            _isDone = true;
+                            return;
+                        }
+                        await Coroutine.Wait(10000, () => !SelectString.IsOpen);
+                    }
+                }
+            }
+            else
+            {
+                if (SelectIconString.IsOpen)
+                {
+                    Logging.WriteDiagnostic($"Choosing '{QuestName}'.");
+                    if (!SelectIconString.ClickLineContains($"{QuestName}"))
+                    {
+                        Logging.WriteDiagnostic($"We can't find quest {QuestName}, exiting'.");
+                        _isDone = true;
+                        return;
+                    }
+                    await Coroutine.Wait(10000, () => !SelectIconString.IsOpen);
+                }
+            }
+            Logging.WriteDiagnostic("Waiting for dialog to open.");
+            await Coroutine.Wait(10000, () => Talk.DialogOpen);
+            while (!SelectString.IsOpen)
+            {
+                Talk.Next();
+                await Coroutine.Yield();
+                await Coroutine.Sleep(500);
+            }
+
+            Logging.WriteDiagnostic("Choosing current class.");
+            ff14bot.RemoteWindows.SelectString.ClickSlot(1);
+            await Coroutine.Wait(10000, () => JournalAccept.IsOpen);
+            {
+                Logging.WriteDiagnostic($"Accepting {QuestName}.");
+                JournalAccept.Accept();
+            }
+            while (!QuestLogManager.HasQuest(QuestId))
+            {
+                if (QuestLogManager.InCutscene)
+                {
+                    TreeRoot.StatusText = "InCutscene";
+                    if (ff14bot.RemoteAgents.AgentCutScene.Instance != null)
+                    {
+                        ff14bot.RemoteAgents.AgentCutScene.Instance.PromptSkip();
+                        await Coroutine.Wait(2000, () => SelectString.IsOpen || SelectYesno.IsOpen);
+
+                        if (SelectString.IsOpen)
+                        {
+                            SelectString.ClickSlot(0);
+                        }
+
+                        if (SelectYesno.IsOpen)
+                        {
+                            SelectYesno.Yes();
+                        }
+                    }
+                }
+
+                Talk.Next();
+                await Coroutine.Wait(1000, () => !Talk.DialogOpen);
+                await Coroutine.Wait(1000, () => Talk.DialogOpen);
+                await Coroutine.Yield();
+            }
+
+            _isDone = true;
+        }
+    }
+}
