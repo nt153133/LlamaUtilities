3c0eba49bf5f2451493213f28f5acf2cc4866c9d
domesticwarlord86 <domesticwarlord86@gmail.com>
2023-02-11 22:02:28 -0600

    Update GCExpertTurnin.cs
    Added handling for leaving GC room

--- OrderbotTags/GCExpertTurnin.cs
+++ OrderbotTags/GCExpertTurnin.cs
@@ -2,7 +2,14 @@
 using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
+using Buddy.Coroutines;
 using Clio.XmlEngine;
+using ff14bot;
+using ff14bot.Behavior;
+using ff14bot.Enums;
+using ff14bot.Managers;
+using ff14bot.Navigation;
+using ff14bot.RemoteWindows;
 using TreeSharp;
 
 namespace LlamaUtilities.OrderbotTags
@@ -47,6 +54,40 @@ namespace LlamaUtilities.OrderbotTags
 
         private async Task DoGCExpertTurnin()
         {
+            if (WorldManager.ZoneId == 534 || WorldManager.ZoneId == 535 || WorldManager.ZoneId == 536)
+            {
+                uint[] npcIds = { 2007528,2006963,2007530 };
+                var exitNpc = GameObjectManager.GameObjects.Where(r => r.IsTargetable && npcIds.Contains(r.NpcId)).OrderBy(r => r.Distance()).FirstOrDefault();
+                if (exitNpc != null)
+                {
+                    while (Core.Me.Location.Distance2D(exitNpc.Location) > 1.5f)
+                    {
+                        await Coroutine.Yield();
+                        Navigator.PlayerMover.MoveTowards(exitNpc.Location);
+                    }
+                    Navigator.PlayerMover.MoveStop();
+                    exitNpc.Interact();
+                    await Coroutine.Wait(10000, () => SelectYesno.IsOpen);
+                    if (!SelectYesno.IsOpen)
+                    {
+                        exitNpc.Interact();
+                        await Coroutine.Wait(10000, () => SelectYesno.IsOpen);
+                    }
+                    while (SelectYesno.IsOpen)
+                    {
+                        SelectYesno.Yes();
+                        await Coroutine.Wait(10000, () => CommonBehaviors.IsLoading);
+                        Log.Information($"Waiting for loading to finish...");
+                        await Coroutine.Wait(-1, () => !CommonBehaviors.IsLoading);
+                    }
+                }
+                else
+                {
+                    Log.Error($"Couldn't find the exit");
+                    return;
+                }
+            }
+
             if (ItemIds.Length == 0)
             {
                 await LlamaLibrary.Helpers.GrandCompanyHelper.GCHandInExpert();
