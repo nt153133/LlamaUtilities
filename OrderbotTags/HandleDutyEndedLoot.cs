f5cb5a194c90381535abb0b6aa868c2636472b51
RedWine82 <redwine@minionway.com>
2023-09-03 22:18:33 -0400

    Feat(HandleDutyEndedLoot): Created new tag that can be used in profiles just before LLLeave to make sure Platypus has time to loot everything, if it was configured to do so.

---
+++ OrderbotTags/HandleDutyEndedLoot.cs
@@ -0,0 +1,61 @@
+﻿namespace LlamaUtilities.OrderbotTags
+{
+    using System.Threading.Tasks;
+    using System.Windows.Media;
+    using Clio.XmlEngine;
+    using LlamaLibrary.Helpers;
+    using TreeSharp;
+
+    [XmlElement("HandleDutyEndedLoot")]
+    public class HandleDutyEndedLoot : LLProfileBehavior
+    {
+        private bool _isDone;
+
+        public override bool HighPriority => true;
+
+        public override bool IsDone => _isDone;
+
+        protected override Color LogColor => Colors.Chartreuse;
+
+        public HandleDutyEndedLoot() : base()
+        {
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnDone()
+        {
+        }
+
+        protected override void OnResetCachedDone()
+        {
+            _isDone = false;
+        }
+
+        protected override Composite CreateBehavior()
+        {
+            return new ActionRunCoroutine(r => HandleDutyEndedLootTask());
+        }
+
+        private async Task HandleDutyEndedLootTask()
+        {
+            if (!PlatypusHelper.HasPlatypus)
+            {
+                Log.Warning("HandleDutyEndedLootTask called but Platypus is not installed so cannot proceed.");
+
+                _isDone = true;
+                return;
+            }
+
+            Log.Information("Duty ended, handling loot via Platypus.");
+
+            await PlatypusHelper.QolOpenTreasureCoffersInDuty();
+
+            await PlatypusHelper.QolWaitUntilAllLootIsGone();
+
+            _isDone = true;
+        }
+    }
+}
