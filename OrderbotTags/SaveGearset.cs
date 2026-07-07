4001105dc4c81ddc5b2a31901ecc2bca49a0dcd9
domesticwarlord86 <domesticwarlord86@gmail.com>
2026-07-07 09:02:04 -0400

    feat: (SaveGearset.cs) Created orderbot tag

---
+++ OrderbotTags/SaveGearset.cs
@@ -0,0 +1,56 @@
+﻿using System.ComponentModel;
+using System.Threading.Tasks;
+using Buddy.Coroutines;
+using Clio.XmlEngine;
+using ff14bot.Behavior;
+using ff14bot.Managers;
+using LlamaLibrary.Helpers;
+using TreeSharp;
+using static LlamaLibrary.Helpers.GeneralFunctions;
+
+namespace LlamaUtilities.OrderbotTags
+{
+    [XmlElement("SaveGearset")]
+    public class SaveGearset : LLProfileBehavior
+    {
+        private bool _isDone;
+
+        public override bool HighPriority => true;
+
+        public override bool IsDone => _isDone;
+
+        public SaveGearset() : base() { }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnDone()
+        {
+        }
+
+        protected override void OnResetCachedDone()
+        {
+            _isDone = false;
+        }
+
+        protected override Composite CreateBehavior()
+        {
+            return new ActionRunCoroutine(r => SaveGearsetTask());
+        }
+
+        private async Task SaveGearsetTask()
+        {
+            if (_isDone)
+            {
+                await Coroutine.Yield();
+                return;
+            }
+
+            await CommonTasks.StopAndDismount();
+            ChatManager.SendChat("/gs save");
+
+            _isDone = true;
+        }
+    }
+}
